cf4c7f773619d811f020e656cce197849c2e285e
jeovane <jeovanearaujo16@gmail.com>
2020-05-02 19:11:44 -0300

    estrutura inicial do projeto, adicionando os arquivos principais

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="App">
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+@font-face {
+    font-family: "Roboto Thin";
+    src: url("./fonts/Roboto-Thin.ttf");
+}
+* {
+    font-family: "Roboto Thin", monospace;
+}
+
+body {
+    margin: 0;
+}
+
+#App {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
