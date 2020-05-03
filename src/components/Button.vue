4793f90fa7bbab502512622147c4db28f8f31a9d
jeovane <jeovanearaujo16@gmail.com>
2020-05-03 15:23:50 -0300

    Logica da calculadora

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,54 @@
 <template>
-    <button class="button">
-        0
+    <button class="button" @click="$emit('onClick', label)" :class="{operation, double, triple}">
+        {{label}}
     </button>
 </template>
 
 <script>
     export default {
-        name: "Button"
+        name: "Button",
+        props: {
+            label: {},
+            operation: { type: Boolean },
+            double: { type: Boolean },
+            triple: { type: Boolean }
+        }
     }
 </script>
 
 <style scoped>
+    :root {
+        --bg-button: #f0f0f0;
+        --border-button: solid 1px #888;
+    }
 
+    .button {
+        font-size: 1.4em;
+        background-color: #f0f0f0;
+        /*border: none;*/
+        border-right: var(--border-button);
+        border-bottom: var(--border-button);
+        outline: none;
+    }
+
+    .button:active {
+        background-color: #ccc;
+    }
+
+    .button.double {
+        grid-column: span 2;
+    }
+
+    .button.triple {
+        grid-column: span 3;
+    }
+
+    .button.operation {
+        background-color: #fa8231;
+        color: #fff;
+    }
+
+    .button.operation:active {
+        background-color: #fa8231cc;
+    }
 </style>
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,15 +1,26 @@
 <template>
     <div class="display">
-        0
+        {{value}}
     </div>
 </template>
 
 <script>
     export default {
-        name: "Display"
+        name: "Display",
+        props: ["value"]
     }
 </script>
 
 <style scoped>
+    .display {
+        grid-column: span 4;
+        background-color: #0004;
 
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 20px;
+        font-size: 2.1em;
+        overflow: hidden;
+    }
 </style>
